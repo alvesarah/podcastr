77267ba64ed95fb615902b6443a38976bd0433a2
alvesarah <sarah.alves8@hotmail.com>
2022-06-26 20:47:41 -0300

    Adicionando slider

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,6 +1,15 @@
+import { useContext } from 'react';
+import { PlayerContext } from '../../contexts/PlayerContext';
+import Image from '../../../node_modules/next/image';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 import styles from './styles.module.scss';
 
 export function Player(){
+    const { episodeList, currentEpisodeIndex } = useContext(PlayerContext)
+
+    const episode = episodeList[currentEpisodeIndex]
+
     return(
         <div className={styles.playerContainer}>
             <header>
@@ -8,15 +17,37 @@ export function Player(){
                 <strong>Tocando agora</strong>
             </header>
 
-            <div className={styles.emptyPlayer}>
-                <strong>Selecione um podcast para ouvir</strong>
-            </div>
+            { episode ? (
+                <div className={styles.currentEpisode}>
+                    <Image
+                        width={592}
+                        height={592}
+                        src={episode.thumbnail}
+                        objectFit="cover"
+                    />
+                    <strong>{episode.title}</strong>
+                    <span>{episode.members}</span>
+                </div>
+            ) : (
+                <div className={styles.emptyPlayer}>
+                    <strong>Selecione um podcast para ouvir</strong>
+                </div>
+            )}
 
-            <footer className={styles.empty}>
+            <footer className={!episode ? styles.empty : ''}>
                 <div className={styles.progress}>
                     <span>00:00</span>
                     <div className={styles.slider}>
-                        <div className={styles.emptySlider} />
+                        { episode ? (
+                            <Slider 
+                                trackStyle={{ backgroundColor: '#04D361'}}
+                                railStyle={{ backgroundColor: '#9F75FF'}}
+                                handleStyle={{ borderColor: '#04D361', borderWidth: 4 }}
+
+                            />
+                        ) : (
+                            <div className={styles.emptySlider} />
+                        )}
                     </div>
                     <span>00:00</span>
                 </div>
