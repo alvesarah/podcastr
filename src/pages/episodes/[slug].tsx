39b81b0f19421d063d1f85f70960fd3ad9fe395c
alvesarah <sarah.alves8@hotmail.com>
2022-06-26 18:41:21 -0300

    Estilizando botões avançar e voltar e adicionando função de voltar para home no episode

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -4,6 +4,7 @@ import { api } from '../../services/api';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import Image from '../../../node_modules/next/image';
+import Link from '../../../node_modules/next/link';
 
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
@@ -29,9 +30,11 @@ export default function Episode({ episode }: EpisodeProps){
     return(
         <div className={styles.episode}>
             <div className={styles.thumbnailContainer}>
-                <button type='button'>
-                    <img src="/arrow-left.svg" alt="Voltar" />
-                </button>
+                <Link href="/">
+                    <button type='button'>
+                        <img src="/arrow-left.svg" alt="Voltar" />
+                    </button>
+                </Link>
                 <Image 
                     width={700}
                     height={160}
