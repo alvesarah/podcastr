4c6f85078d2cfbd952cec6d60d7990e7762a039b
alvesarah <sarah.alves8@hotmail.com>
2022-06-27 00:33:12 -0300

    Colocando title interatico e icon

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -4,6 +4,7 @@ import { api } from '../../services/api';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import Image from '../../../node_modules/next/image';
+import Head from 'next/head';
 import Link from '../../../node_modules/next/link';
 
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
@@ -32,6 +33,10 @@ export default function Episode({ episode }: EpisodeProps){
     
     return(
         <div className={styles.episode}>
+            <Head>
+                <title>{episode.title} | Podcastr</title>
+            </Head>
+
             <div className={styles.thumbnailContainer}>
                 <Link href="/">
                     <button type='button'>
